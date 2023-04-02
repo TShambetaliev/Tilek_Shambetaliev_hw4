88c820fbd83af00eaf7997f624469befa8fa4045
Tshambetaliev <tshambetaliev@gmail.com>
2023-04-02 16:36:55 +0600

    HW 5, fun bind, Taks Adapter

--- app/src/main/java/com/example/tilek_shambetaliev_hw4/ui/home/adapter/TaskAdapter.kt
+++ app/src/main/java/com/example/tilek_shambetaliev_hw4/ui/home/adapter/TaskAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.tilek_shambetaliev_hw4.ui.home.adapter
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.tilek_shambetaliev_hw4.R
 import com.example.tilek_shambetaliev_hw4.databinding.ItemTaskBinding
 import com.example.tilek_shambetaliev_hw4.model.Task
 
@@ -34,6 +37,7 @@ class TaskAdapter(private val onClik: (Task) -> Unit) : Adapter<TaskAdapter.Task
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         holder.bind(data[position])
+        holder.bind(position)
     }
 
     override fun getItemCount(): Int = data.size
@@ -45,6 +49,18 @@ class TaskAdapter(private val onClik: (Task) -> Unit) : Adapter<TaskAdapter.Task
             itemView.setOnLongClickListener {
                 onClik(task)
                 false
+            }
+        }
+
+        fun bind(position: Int) {
+            if (position % 2 == 0) {
+                itemView.setBackgroundColor(Color.BLACK)
+                itemView.findViewById<TextView>(R.id.tv_title).setTextColor(Color.WHITE)
+                itemView.findViewById<TextView>(R.id.tv_desc).setTextColor(Color.WHITE)
+            } else {
+                itemView.setBackgroundColor(Color.WHITE)
+                itemView.findViewById<TextView>(R.id.tv_title).setTextColor(Color.BLACK)
+                itemView.findViewById<TextView>(R.id.tv_desc).setTextColor(Color.BLACK)
             }
         }
     }
